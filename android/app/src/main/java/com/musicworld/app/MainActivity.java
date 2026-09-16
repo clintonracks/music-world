63129cee122c4d71c443631b7e1e44dfdad8eac7
Clinton Bwalya <bwalyaclinton12@gmail.com>
2026-09-16 18:08:00 +0200

    Register Device Music plugin before bridge

--- android/app/src/main/java/com/musicworld/app/MainActivity.java
+++ android/app/src/main/java/com/musicworld/app/MainActivity.java
@@ -2,11 +2,12 @@ package com.musicworld.app;
 
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
+import java.util.Arrays;
 
 public class MainActivity extends BridgeActivity {
     @Override
-    public void onCreate(android.os.Bundle savedInstanceState) {
+    protected void onCreate(android.os.Bundle savedInstanceState) {
+        registerPlugins(Arrays.asList(DeviceMusicPlugin.class));
         super.onCreate(savedInstanceState);
-        registerPlugin(DeviceMusicPlugin.class);
     }
 }
